33455d800add9dab560858b8ebbb03e5550f2b1f
MarvinNamanyaUdahKepakai <157597148+MarvinNamanyaUdahKepakai@users.noreply.github.com>
2024-02-29 14:20:56 +0700

    Update transaksi.php

--- app/Views/transaksi.php
+++ app/Views/transaksi.php
@@ -1,4 +1,4 @@
-<div class="content-wrapper">
+<!-- <div class="content-wrapper">
     <div class="container-xxl flex-grow-1 container-p-y">
         <h4 class="fw-bold py-3 mb-2"><span>Daftar Transaksi</h4>
         <div class="card mb-3">
@@ -106,4 +106,4 @@
         document.getElementById('deleteLink').href = deleteLink;
         $('#deleteModal').modal('show');
     }
-    </script>
+    </script> -->
